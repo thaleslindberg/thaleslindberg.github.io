5588d08a9f3c18e1506818a8b0c3c364dd0cdb0a
Thales Lindberg <thaleslindberg@Matrix.local>
2022-02-07 18:47:17 -0300

    changes

--- js/index.js
+++ js/index.js
@@ -2,5 +2,9 @@ setInterval(showTime, 1000)
         
 function showTime() {
     const time = new Date()
-    document.getElementById("clock").innerHTML = time.toLocaleDateString()
+    document.getElementById("clock").innerHTML = time.toLocaleTimeString()
+
+    // document.getElementById("clock").innerHTML = time.toLocaleDateString()
+    // document.getElementById("clock").innerHTML = time.getFullYear() + " " + time.toLocaleTimeString()
 }
+
