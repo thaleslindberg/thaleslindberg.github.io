a9b81056875ed1a30af79fe7dcb2a7e45c35eed9
thaleslindberg <thales.lindberg@gmail.com>
2024-08-03 17:10:53 -0400

    changes

--- js/index.js
+++ js/index.js
@@ -21,5 +21,49 @@ window.addEventListener('load', () => {
   window.addEventListener('resize', updateScroll, false);
 });
 
+// Select all elements with the class 'item' inside 'accordion'
+const items = document.querySelectorAll('.accordion .item');
 
+// Define an array of background images for each item
+const backgroundImages = [
+    "/img/project-1.png", // Background image for the first item
+    "/img/project-2.png", // Background image for the second item
+    "/img/project-3.png", // Background image for the third item
+    "/img/project-4.png", // Background image for the fourth item
+    "/img/project-5.png", // Background image for the fifth item
+    "/img/project-6.png" // Background image for the sixth item
+];
 
+// Define an array of URLs for each item
+const urls = [
+    '/pages/corporate.html', // URL for the first item
+    '/pages/global.html', // URL for the second item
+    '/pages/power_skills.html', // URL for the third item
+    '/pages/le_septieme.html', // URL for the fourth item
+    '/pages/ux_case.html', // URL for the fifth item
+    '/pages/nouvelle.html'  // URL for the sixth item
+];
+
+// Function to add event listeners to each item
+items.forEach((item, index) => {
+    // Add hover event listeners
+    item.addEventListener('mouseover', () => {
+        if (backgroundImages[index]) {
+            item.style.backgroundImage = `url('${backgroundImages[index]}')`;
+            item.style.backgroundRepeat = 'no-repeat';
+            item.style.backgroundSize = 'cover';
+            item.style.backgroundPosition = 'center center';
+            item.style.transition = 'all 1s ease-out';
+        }
+    });
+    
+    item.addEventListener('mouseout', () => {
+        item.style.backgroundImage = '';
+    });
+
+    // Add click event listener to navigate to the URL
+    item.addEventListener('click', () => {
+        // window.location.href = urls[index];
+        window.open(urls[index], '_blank');
+    });
+});
